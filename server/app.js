352d191cd31e171cc55ea978176d8b1783a8f58b
Dmytro Khomiak <d.khomiak@sana-commerce.com>
2019-09-26 12:49:22 +0300

    lesson 8

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
-const express = require("express");
-const graphqlHTTP = require("express-graphql");
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const PORT = 4000;
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({
+  schema,
+}));
 
 app.listen(PORT, () => {
   console.log(`server is started on port ${PORT}`);
