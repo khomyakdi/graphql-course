a4bac3db02c3c3fdc3650c1c920d0f865a84b5ce
khomyakdi <khomyakdi@gmail.com>
2019-09-29 19:55:53 +0300

    lesson 16

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const PORT = 4000;
+const MONGODB_CONNECTION_STRING = 'momongodb://localhost:27017/gql-ninja';
 
 const app = express();
+
+mongoose.connect(MONGODB_CONNECTION_STRING, () => {
+  console.log('connected');
+});
 
 app.use(
   '/graphql',
