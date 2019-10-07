6f8aaf7f23fe656b325a45976ab5f3796f785b70
khomyakdi <khomyakdi@gmail.com>
2019-10-07 22:02:40 +0300

    lesson 24

---
+++ client/src/components/BookList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BookList = props => {
+  return (
+    <div>
+      <ul id="book-list">
+        <li>book </li>
+      </ul>
+    </div>
+  );
+};
+
+BookList.propTypes = {
+
+};
+
+export default BookList;
